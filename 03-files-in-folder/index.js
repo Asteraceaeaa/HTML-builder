1ea71f4247a61614e259cecd512b5bb653df6efe
Asteraceaeaa <kovinv454@gmail.com>
2024-01-22 08:35:16 +0500

    1

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { access } = require('fs/promises');
 const path = require('path');
 const folderPath = path.join(__dirname, 'secret-folder');
 fs.readdir(folderPath, (err, files) => {
@@ -8,18 +9,50 @@ fs.readdir(folderPath, (err, files) => {
   }
 
   files.forEach((file) => {
-    fs.stat(folderPath, (err, stats) => {
+    let currentPath = path.join(folderPath, file);
+    fs.stat(currentPath, (err, stats) => {
       if (err) {
         console.error('Error getting file stats:', err);
         return;
       }
+      if (isDir(currentPath)) {
+        fs.readdir(currentPath, (err, files) => {
+          if (err) {
+            console.error('Error while reading directory:', err);
+            return;
+          }
 
-      const fileSizeInBytes = stats.size;
-      const fileSizeInKB = fileSizeInBytes / 1024;
-
-      console.log(
-        `${file}-${path.extname(file).slice(1)}-${fileSizeInKB.toFixed()}kb`,
-      );
+          files.forEach((file) => {
+            let currentPath = path.join(folderPath, file);
+            fs.stat(path.join(currentPath, file), (err, stats) => {
+              if (err) {
+                console.error('Error getting file stats:', err);
+                return;
+              } else
+                console.log(
+                  `${file}-${path.extname(file).slice(1)}-${stats.size}Bytes`,
+                );
+            });
+          });
+        });
+      } else {
+        console.log(
+          `${file}-${path.extname(file).slice(1)}-${stats.size}Bytes`,
+        );
+      }
     });
   });
 });
+
+function isDir(path) {
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error('Error getting file stats:', err);
+      return;
+    }
+    if (stats.isDirectory()) {
+      return true;
+    }
+    return false;
+  });
+}
